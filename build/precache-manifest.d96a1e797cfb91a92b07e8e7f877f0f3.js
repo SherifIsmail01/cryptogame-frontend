aa242c52a1c372f8947292c6ad84e98f0b03931d
Sherif Ismail <sherifismail01@gmail.com>
2020-01-12 13:41:01 -0800

    build production env.

---
+++ build/precache-manifest.d96a1e797cfb91a92b07e8e7f877f0f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d295c222cbef5310c47326684518d8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "985353082dcb37863f4c",
+    "url": "/static/css/2.d5831cfa.chunk.css"
+  },
+  {
+    "revision": "b8f2f2b7f080fc7423e7",
+    "url": "/static/css/main.1e5082d9.chunk.css"
+  },
+  {
+    "revision": "985353082dcb37863f4c",
+    "url": "/static/js/2.7f66978b.chunk.js"
+  },
+  {
+    "revision": "1a974fcef351019892557fbd6e232f86",
+    "url": "/static/js/2.7f66978b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8f2f2b7f080fc7423e7",
+    "url": "/static/js/main.f88a1095.chunk.js"
+  },
+  {
+    "revision": "600f84fe4a74a50d394f",
+    "url": "/static/js/runtime-main.57b6c16c.js"
+  }
+]);
